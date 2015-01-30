a209ac27d5ac013f0a4a3ef5cb15f3e443982db9
mmedina <mmedina@nearsoft.com>
2015-01-30 12:06:54 -0700

    report styles and bug fixes

--- page-report.php
+++ page-report.php
@@ -6,8 +6,8 @@ Template Name: REPORT
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-  <section class="report-hero" style="url(<?php the_field('report_hero_background_image'); ?>) no-repeat;">
-    <?php the_field('report_hero_client_logo'); ?>
+  <section class="report-hero" style="background: url(<?php the_field('report_hero_background_image'); ?>) repeat;">
+    <img src="<?php the_field('report_hero_client_logo'); ?>">
     <p><?php the_field('report_hero_description'); ?></p>
     <h3>-Design Method-</h3>
     <span class="designmethod"><?php the_field('report_hero_design_method'); ?></span>
@@ -29,10 +29,10 @@ Template Name: REPORT
     <h2><?php the_field('report_download_label'); ?></h2>
     <a class="button download-btn" href="<?php the_field('report_download_button_url'); ?>"><?php the_field('report_download_button_label'); ?></a>
   </section>
-  
+
   <section class="report-pagination">
   </section>
-  
+
   <?php include 'inc-submit.php'; ?>
 
 <?php endwhile; ?><?php endif; ?>
