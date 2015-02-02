2aaf57a4c0a42d5493f863ef6f7181273fad5c66
squisbiz <arturoruiz@Arturos-MacBook-Pro-3.local>
2015-02-01 21:20:58 -0700

    page episode basic structure

--- page-episodes.php
+++ page-episodes.php
@@ -6,13 +6,28 @@ Template Name: EPISODES INDEX
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php
-  query_posts('cat=3');
-  while (have_posts()) : the_post(); ?>
-  <h2><?php the_title(); ?></h2>
-  <?php endwhile; ?>
+<div class="episodesindex">
+  <h1>Episodes</h1>
+  <div class="content">
+    <?php
+      query_posts('cat=3');
+      while (have_posts()) : the_post(); ?>
+      <section>
+        <div class="thumb" style="background: url('<?php $thumb_id = get_post_thumbnail_id(); $thumb_url = wp_get_attachment_image_src($thumb_id,'medium', true); echo $thumb_url[0]; ?>') no-repeat center center;">
+        
+        <div class="excerpt">
+          <h2><?php the_title(); ?></h2>
+        </div>
+        
+      </section>
+      <?php endwhile; ?>
+    <?php endwhile; ?><?php endif; ?>
+  </div>
 
-  <?php include 'inc-submit.php'; ?>
+  <div class="sidebar"></div>
+</div>
 
-<?php endwhile; ?><?php endif; ?>
+
+<?php include 'inc-submit.php'; ?>
+
 <?php get_footer(); ?>
