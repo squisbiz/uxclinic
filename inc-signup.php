016fba19125c9bc8faf081b3715aaf3984711ba9
squisbiz <arturoruiz@Arturos-MacBook-Pro-3.local>
2015-02-03 11:28:11 -0700

    episode index changes

--- inc-signup.php
+++ inc-signup.php
@@ -1,6 +1,6 @@
 <section class="submit signup">
-    <h1>Subscribe copy from Jorge</h1>
-    <p>Generative Research is a series of collaborative activities involving people (designers and non-designers) using tangible.</p>
+    <h1>Don't miss any episode. Suscribe to our newsletter.</h1>
+    <p>Get useful UX insights from the feedback and recommendations our panel of experts will be giving. We will never sell your email address to any 3rd party or send you nasty spam. Promise.</p>
     <!-- Begin MailChimp Signup Form -->
   <div id="mc_embed_signup">
   <form action="//nearsoft.us4.list-manage.com/subscribe/post?u=639555ce3b92a7d8f227cf256&amp;id=b2ec3b8652" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
