f3b6516993641c4411bccedabe05cff474cffc30
mmedina <mmedina@nearsoft.com>
2015-02-04 11:19:28 -0700

    page episodes style adjustments and home bugs

--- page-episodes.php
+++ page-episodes.php
@@ -7,21 +7,22 @@ Template Name: EPISODES INDEX
 
 
 <div class="episodesindex">
-  
-  <h1>Episodes</h1>
-  
+  <div class="cont-h1">
+    <h1>Episodes</h1>
+  </div>
+
   <div class="the_content">
-    
+
     <?php query_posts('cat=3'); while (have_posts()) : the_post(); ?>
-    
+
       <div class="single">
         <a href="<?php the_permalink(); ?>"><div class="thumb" style="background: url(<?php $thumb_id = get_post_thumbnail_id(); $thumb_url = wp_get_attachment_image_src($thumb_id,'medium', true); echo $thumb_url[0]; ?>) no-repeat center center; background-size: cover;"></div></a>
-        
+
         <div class="excerpt">
           <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
           <?php the_excerpt(); ?>
         </div>
-        
+
       </div>
       <?php endwhile; ?>
 
