303e6dc953ecb22aeb0385ade98cbc6e09d56378
squisbiz <arturoruiz@Arturos-MacBook-Pro.local>
2015-02-17 17:52:03 -0700

    404 empty fields

--- 404.php
+++ 404.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
 	<!-- 404 content -->
+	<section class="error404">
+  	 <p><?php the_field('404_copy'); ?></p>
+  	 <a class="button" href="<?php the_field('404_button_url'); ?>"><?php the_field('404_button_label'); ?></a>
+	</section>
 
 <?php get_footer(); ?>
