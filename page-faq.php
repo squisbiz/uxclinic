1b7c7b8710ffdf0a7394275bdf0410f5ba726eb2
mmedina <mmedina@nearsoft.com>
2015-02-18 17:46:21 -0700

    faq page missing styles

--- page-faq.php
+++ page-faq.php
@@ -7,14 +7,17 @@ Template Name: FAQ
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
   <section class="faq">
-    <h1><?php echo get_the_title(); ?></h1>
-    
-    <?php the_content(); ?>
-
+    <div class="cont-h1">
+        <h1><?php echo get_the_title(); ?></h1>
+    </div>
   </section>
-  
+
+  <section class="faq-content">
+    <?php the_content(); ?>
+  </section>
+
   <?php include 'inc-signup.php'; ?>
-  
+
   <?php include 'inc-episodes.php'; ?>
 
 <?php endwhile; ?><?php endif; ?>
