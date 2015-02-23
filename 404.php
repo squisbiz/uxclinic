0c68667a1231c09f0bcca23f8eece9dfb9a12d4c
squisbiz <arturoruiz@Arturos-MacBook-Pro.local>
2015-02-23 10:07:25 -0700

    404 change
    removed custom fields

--- 404.php
+++ 404.php
@@ -1,9 +1,8 @@
 <?php get_header(); ?>
 
-	<!-- 404 content -->
-	<section class="error404">
-  	 <p><?php the_field('404_copy'); ?></p>
-  	 <a class="button" href="<?php the_field('404_button_url'); ?>"><?php the_field('404_button_label'); ?></a>
+  <section class="error404">
+  	 <p>404 copy here</p>
+  	 <a class="button" href="#">404 button label</a>
 	</section>
 
 <?php get_footer(); ?>
