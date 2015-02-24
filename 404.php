bd4196523da388901da62fbe1dccf8882a134286
mmedina <mmedina@nearsoft.com>
2015-02-24 16:35:47 -0700

    404 error page change

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 	<div>
 		 <h1>404</h1>
 		 <p>We didn’t find the page you were looking for...</p>
-		 <a class="button" href="#">Take me home</a>
+		 <a class="button" href="<?php echo home_url(); ?>">Take me home</a>
 	</div>
 	</section>
 
