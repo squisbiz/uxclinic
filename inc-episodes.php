8619231fce8389fa6da70c07c5c4e550a31d9384
squisbiz <arturoruiz@Arturos-MacBook-Pro.local>
2015-03-20 16:05:53 -0700

    last fix

--- inc-episodes.php
+++ inc-episodes.php
@@ -1,6 +1,9 @@
 <section class="episodes">
+  <?php query_posts('page_id=2'); if (have_posts()) : while (have_posts()) : the_post(); ?>
   <h2><?php the_field('home_episodes_title'); ?></h2>
   <a class="button" href="http://theuxclinic.com/episodes">See all episodes</a>
+  <?php endwhile; ?><?php endif; WP_reset_query(); ?>
+  
   <ul>
     <?php query_posts( array( 'post_type' => 'uxclinic_episode', 'posts_per_page' => 3) ); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <li>
