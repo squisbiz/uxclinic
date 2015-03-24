35d7610b92f919bf2e4fdc4e767f406ec3d496fd
mmedina <mmedina@nearsoft.com>
2015-03-24 16:57:18 -0700

    about us page link 2

--- page-about.php
+++ page-about.php
@@ -20,13 +20,13 @@ Template Name: ABOUT
     <div class="left-column">
       <h2><?php the_field('about_content_title'); ?></h2>
       <p><?php the_field('about_content_description'); ?></p>
+      <a id="link-anchor2" class="scnd-case-link">Submit your case study</a>
     </div>
   </section>
 
 </div>
 <?php include 'inc-experts.php'; ?>
 <?php include 'inc-submit.php'; ?>
-
 <?php get_footer(); ?>
 <script>
   function scrollToAnchor(aid){
@@ -35,6 +35,9 @@ Template Name: ABOUT
 }
 
 $("#link-anchor").click(function() {
+   scrollToAnchor('link-anchor');
+});
+$("#link-anchor2").click(function() {
    scrollToAnchor('link-anchor');
 });
 </script>
