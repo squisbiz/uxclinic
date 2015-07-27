516fe0bd87d7adae399b4df750ad60ddd94967db
squisbiz <arturoruiz@Arturos-MacBook-Pro.local>
2015-07-27 16:46:04 -0700

    blog feed fix

--- category.php
+++ category.php
@@ -7,9 +7,8 @@
   
   <div class="blogposts">
 
-  <?php $the_query = new WP_Query( 'posts_per_page=10' ); ?>
-  
-  <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+  <?php if ( have_posts() ) : ?>
+  <?php while ( have_posts() ) : the_post();  ?>
   
     <a href="<?php the_permalink() ?>" class="loop-single">
       <div class="thumb" style="background: url(<?php $thumb_id = get_post_thumbnail_id(); $thumb_url = wp_get_attachment_image_src($thumb_id,'full', true); echo $thumb_url[0]; ?>) no-repeat center center; background-size: cover;"></div>
@@ -19,10 +18,16 @@
             <?php the_excerpt() ?>
           </div>
     	</div>
-  
     </a>
   
-  <?php endwhile; wp_reset_postdata(); ?>
+  <?php endwhile; ?>
+  
+  <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
+  <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+  
+  <?php else : ?>
+  <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+  <?php endif; ?>
   
   </div>
 </div>
