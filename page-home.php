46618080ca07de6d3bb7baf089097744d68ca99a
mmedina <mmedina@nearsoft.com>
2015-11-19 11:45:14 -0700

    home changes

--- page-home.php
+++ page-home.php
@@ -7,40 +7,54 @@ Template Name: HOME
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-  <section class="hero" style="background: url(<?php the_field('home_hero_image_bg'); ?>) no-repeat center top, url(http://nearsoft.com/stg.uxclinic/wp/wp-content/themes/uxclinic/img/ux-lab-background.gif) repeat-x center bottom;">
+  <section class="hero" style="background: url(<?php the_field('home_hero_image_bg'); ?>) no-repeat center top, url(http://theuxclinic.com/wp/wp-content/uploads/2015/11/ux-research-designers-home.jpg) no-repeat center bottom; background-size: cover;">
     <h1><?php the_field('home_hero_title'); ?></h1>
 
 <?php the_field('home_hero_text'); ?>
-  <p><a id="link-anchor" href="<?php the_field('home_hero_button_url'); ?>" class="button big-button"><?php the_field('home_hero_button_text'); ?></a></p>
+<div class="services-cont">
+  <div class="blocks">
+     <img src="http://theuxclinic.com/wp/wp-content/uploads/2015/11/user-research-uxteam.jpg">
+     <div class="block-text-cont">
+        <h2>User Research</h2>
+        <p>Listening to your users is key to understand their needs and wants.</p>
+     </div>
+  </div>
+    <div class="blocks">
+      <img src="http://theuxclinic.com/wp/wp-content/uploads/2015/11/data-interpretation-uxteam.jpg">
+     <div class="block-text-cont">
+        <h2>Data Interpretation</h2>
+        <p>User insights are translated into design features for your products.</p>
+     </div>
+  </div>
+    <div class="blocks">
+       <img src="http://theuxclinic.com/wp/wp-content/uploads/2015/11/ux-solutions-uxteam.jpg">
+     <div class="block-text-cont">
+        <h2>UX Solutions</h2>
+        <p>We create visual solutions ready to be implemented in your next design iteration.</p>
+     </div>
+  </div>
+
+</div>
+  <p><a id="link-anchor" href="<?php the_field('home_hero_button_url'); ?>" class="contactform-submit"><?php the_field('home_hero_button_text'); ?></a></p>
 
   </section>
 
-  <section class="howitworks">
-    <h2><?php the_field('home_how_title'); ?></h2>
-    <ol>
-      <?php if( have_rows('home_how_steps') ) : while ( have_rows('home_how_steps') ) : the_row(); ?>
-      <li>
-        <img src="<?php the_sub_field('how_image');?>" alt="<?php the_sub_field('how_image_alt');?>" />
-        <h3><?php the_sub_field('how_title');?></h3>
-        <?php the_sub_field('how_description');?>
-      </li>
-      <?php endwhile; else : endif; ?>
-    </ol>
-  </section>
-
-  <section class="experts">
-    <h2><?php the_field('home_experts_title'); ?></h2>
-    <h3>Our team is an expert in researching how user feedback can make a product functional and delightful.</h3>
-    <a class="button">See Experts</a>
+  <section class="episodes">
+    <h2><?php the_field('home_episodes_title'); ?></h2>
+    <a class="button" href="http://theuxclinic.com/casestudies">See all Case studies</a>
     <ul>
-      <?php if( have_rows('experts_experts') ) : while ( have_rows('experts_experts') ) : the_row(); ?>
-      <li style="background: url(<?php the_sub_field('expert_pic');?>); background-position-x: center;">
-        <h3><?php the_sub_field('expert_name');?></h3>
-        <?php the_sub_field('expert_bio');?>
+      <?php query_posts( array( 'post_type' => 'uxclinic_casestudy', 'posts_per_page' => 3) ); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <li>
+        <a href="<?php the_permalink(); ?>">
+          <div class="image-container" style="background: url(<?php the_field('episode_featured_image'); ?>); background-size: cover;">
+          </div>
+
+          <h3><?php the_field('episode_featured_episode_number');?><span><?php the_field('episode_featured_episode_client');?></span></h3>
+          <p><?php the_field('episode_featured_episode_description');?></p>
+        </a>
       </li>
-      <?php endwhile; else : endif; ?>
+      <?php endwhile; endif; wp_reset_query(); ?>
     </ul>
-    <a name="link-anchor"></a>
   </section>
 
   <section class="submit">
@@ -60,26 +74,6 @@ Template Name: HOME
   </section>
 
 
-  <section class="episodes">
-    <h2><?php the_field('home_episodes_title'); ?></h2>
-    <a class="button" href="http://theuxclinic.com/casestudies">See all Case studies</a>
-    <ul>
-      <?php query_posts( array( 'post_type' => 'uxclinic_casestudy', 'posts_per_page' => 3) ); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <li>
-        <a href="<?php the_permalink(); ?>">
-          <span class="vid-flag"></span>
-
-          <div class="image-container" style="background: url(<?php the_field('episode_featured_image'); ?>); background-size: cover;">
-
-          </div>
-
-          <h3><?php the_field('episode_featured_episode_number');?><span><?php the_field('episode_featured_episode_client');?></span></h3>
-          <p><?php the_field('episode_featured_episode_description');?></p>
-        </a>
-      </li>
-      <?php endwhile; endif; wp_reset_query(); ?>
-    </ul>
-  </section>
 
 <?php endwhile; ?><?php endif; ?>
 <?php get_footer(); ?>
@@ -92,5 +86,22 @@ Template Name: HOME
 
 $("#link-anchor").click(function() {
    scrollToAnchor('link-anchor');
+});
+</script>
+<script>
+  // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+var mainbottom = $('.hero').offset().top + $('.hero').height();
+// on scroll,
+$(window).on('scroll',function(){
+
+    // we round here to reduce a little workload
+    var stop = Math.round($(window).scrollTop());
+
+    if (stop > mainbottom) {
+        $('header').addClass('past-main').fadeIn("slow");
+
+    } else {
+        $('header').removeClass('past-main');
+    }
 });
 </script>
