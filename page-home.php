95462850e4324d033cc07b272b638f6a355c7a77
mmedina <mmedina@nearsoft.com>
2015-12-02 17:12:45 -0700

    changes uxclinic site case studies

--- page-home.php
+++ page-home.php
@@ -40,16 +40,17 @@ Template Name: HOME
 
   <section class="episodes">
     <h2><?php the_field('home_episodes_title'); ?></h2>
-    <a class="button" href="http://theuxclinic.com/casestudies">See all Case studies</a>
+    <div class="our-client-text">We have helped several software companies improve their products. <a href="http://theuxclinic.com/casestudies">See all Case studies</a></div>
     <ul>
-      <?php query_posts( array( 'post_type' => 'uxclinic_casestudy', 'posts_per_page' => 3) ); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php query_posts( array( 'post_type' => 'uxclinic_casestudy', 'posts_per_page' => 2) ); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <li>
         <a href="<?php the_permalink(); ?>">
           <div class="image-container" style="background: url(<?php the_field('episode_featured_image'); ?>); background-size: cover;">
+          <h3><?php the_field('episode_featured_episode_number');?></h3>
+          <p>
+            <span><?php the_field('episode_featured_episode_client');?></span>
+            <?php the_field('episode_featured_episode_description');?></p>
           </div>
-
-          <h3><?php the_field('episode_featured_episode_number');?><span><?php the_field('episode_featured_episode_client');?></span></h3>
-          <p><?php the_field('episode_featured_episode_description');?></p>
         </a>
       </li>
       <?php endwhile; endif; wp_reset_query(); ?>
