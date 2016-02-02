01bcfe77671eeb61f0fc148b7aa80edcc2e301fa
squisbiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2016-02-02 10:06:32 -0700

    fixes

--- page-contact.php
+++ page-contact.php
@@ -29,7 +29,7 @@ Template Name: CONTACT PAGE
         <input type="text" class="name" id="name" name="name" placeholder="Name">
         <input type="text" class="email" id="email" name="email" placeholder="E-mail">
         <input type="text" class="telephone" id="telephone" name="telephone" placeholder="Phone (optional) [xxx-xxx-xxxx]">
-        <textarea id="Description" name="Description" class="message"placeholder="Your Message"></textarea>
+<!--         <textarea id="Description" name="Description" class="message"placeholder="Your Message"></textarea> -->
         <p class="center"><input type="button" value="Send request" class="button" onclick="requestSubmit()"><a href="#" class="close"><i class="icon-remove"></i></a></p>
         <input type="hidden" id="company" name="company" value="nearsoft">
       </form>
