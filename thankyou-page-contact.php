f19dcdd87967dba9d9665a10544547e308d43b99
squisbiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2016-02-08 09:32:03 -0700

    button label changes

--- thankyou-page-contact.php
+++ thankyou-page-contact.php
@@ -9,7 +9,7 @@ Template Name: Thank you page contact
   <div>
      <h1>Thanks!</h1>
      <p>Thanks for your contact request. We’ll contact you shortly.</p>
-     <a class="button" href="http://ux.nearsoft.com/casestudies/reduce-lack-of-engagement-and-lower-abandonment-rate/">Take me home</a>
+     <a class="button" href="http://ux.nearsoft.com/casestudies/reduce-lack-of-engagement-and-lower-abandonment-rate/">Our Work</a>
   </div>
   </section>
 
