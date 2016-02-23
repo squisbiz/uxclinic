978ee7ec75e11ed200d73f341f471f19737d43da
squisbiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2016-02-23 10:06:36 -0700

    experts changes

--- inc-experts.php
+++ inc-experts.php
@@ -12,6 +12,10 @@
         <h2>Misael Leon</h2>
         <h3>UX Researcher</h3>
         <p>Misael enjoys turning complex systems into simple experiences by throwing users into the mix. He does it through Design Thinking and User Research.</p>
+        <div class="social-box">
+          <a href="https://mx.linkedin.com/in/misaelleon/en" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/misaello" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
@@ -23,6 +27,10 @@
         <h2>Diana Arvayo</h2>
         <h3>UX Researcher</h3>
         <p>Diana is an expert designing research-based experiences and solutions, bringing to life what users expect and service providers need.</p>
+        <div class="social-box">
+          <a href="https://mx.linkedin.com/in/darvayo/en" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/dianonina" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
@@ -34,6 +42,10 @@
         <h2>Miguel Medina</h2>
         <h3>UX/UI Designer</h3>
         <p>Miguel has over five years of experience in the web industry. His goal is to create beautifully functional interfaces to tend to his users' needs.</p>
+        <div class="social-box">
+          <a href="https://www.linkedin.com/in/miguel-angel-medina-fontes-9b818a4a" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/MiigueelMediina" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
@@ -45,6 +57,10 @@
         <h2>Arturo Ruiz</h2>
         <h3>UI Designer</h3>
         <p>Arturo is equally a coder and a designer. His strength lies in adapting beautiful UX oriented visual solutions to sophosticated UIs.</p>
+        <div class="social-box">
+          <a href="https://mx.linkedin.com/in/arturo-ruiz-05b25023" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/squisbiz" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
@@ -56,6 +72,10 @@
         <h2>Denisse Gutiérrez</h2>
         <h3>UX Researcher</h3>
         <p>Denisse has a human centered design approach to products. She thrives to understand how users interact with products to create a positive experience.</p>
+        <div class="social-box">
+          <a href="https://www.linkedin.com/in/neniza" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/neniza_" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
@@ -67,6 +87,10 @@
         <h2>Axel Valdez</h2>
         <h3>UI Designer</h3>
         <p>Axel has been a designgineer for two decades. His main focus has been UI with an interest for UX. He speaks HTML, CSS and JavaScript.</p>
+        <div class="social-box">
+          <a href="https://www.linkedin.com/in/axelvaldez" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/axel" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
@@ -78,6 +102,10 @@
         <h2>Sandra Vazquez</h2>
         <h3>Business Developer</h3>
         <p>Sandra has created business since she first entered the Startup scene in 2009. Her main strength is company growth with a focus on product development.</p>
+        <div class="social-box">
+          <a href="https://mx.linkedin.com/in/sandradanielav" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/sandra_daniela" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
@@ -89,6 +117,10 @@
         <h2>Jorge Symonds</h2>
         <h3>Content Creator</h3>
         <p>Jorge is a professional storyteller for brand and products. He finds the best medium to generate content and distribute it.</p>
+        <div class="social-box">
+          <a href="https://www.linkedin.com/in/jorge-luis-symonds-pavlovich-6904b15a/en" target="_blank"><div class="social-link linkedin"></div></a>
+          <a href="http://twitter.com/jorgepistola" target="_blank"><div class="social-link twitter"></div></a>
+        </div>
       </div>
   </div>
   
