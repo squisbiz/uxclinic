cac26110c8e6a00071a967e60d063fac2ac80434
squisbiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2016-03-02 14:47:17 -0700

    meta description

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+  <meta name="description" content="We help software companies accelerate their business by powering up their user experience" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
