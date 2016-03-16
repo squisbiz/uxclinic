7affb7cd3537134defa98bf19f4989129ac41c07
squisbiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2016-03-16 16:03:14 -0700

    workshop landing page

---
+++ page-landingpage-adroll.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: LANDING PAGE AD ROLL
+*/
+?>
+<?php get_header(); ?>
+
+<section class="e-hero">
+  <h1>User Experience Makes a Difference in Software Development</h1>
+</section>
+
+<section class="e-can">
+  <h2>UX Can Have a Lasting Impact in Your Company</h2>
+  <h3>Our UX Team Can Help You To:</h3>
+  <div class="e-can-container">
+    <div class="e-can-item">
+      <div class="e-can-image">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/intuitive-design-btn.png" />
+      </div>
+      <div class="e-can-text">Make Your Products More Intuitive</div>
+    </div>
+    
+    <div class="e-can-item">
+      <div class="e-can-image">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/user-satisfaction.png" />
+      </div>
+      <div class="e-can-text">Increase Customer Satisfactions</div>
+    </div>
+    
+    <div class="e-can-item">
+      <div class="e-can-image">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/user-engagement.png" />
+      </div>
+      <div class="e-can-text">Boost Your Engagement</div>
+    </div>
+    
+  </div>
+</section>
+
+<div class="footer-padding-div"></div>
+
+
+
+<?php get_footer(); ?>
