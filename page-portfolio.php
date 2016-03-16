b7163a5674bd36afbba7ae5c261df1a8c6441e1c
squisbiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2016-03-15 22:02:37 -0700

    portfolio and landing page

---
+++ page-portfolio.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: PORTFOLIO
+*/
+?>
+<?php get_header(); ?>
+
+<div class="porfolio-main">
+
+
+<section class="p-hero">
+  <h1>UX Team Portfolio</h1>
+</section>
+
+<section class="p-about">
+  <h2>About Us</h2>
+  <h3>We help software companies accelerate their business by powering up their user experience.</h3>
+  <h3 class="orange-title">How we do it</h3>
+  <p>We talk directly with your users and discover how they interact with your product or service. conduct personalized UX methodologies and activities to understand your user's wants and needs.</p>
+  <p>We gather insights and translate them into visual solutions.</p>
+  <p>We work hand in hand with your development team to make the implementation process seamless to ultimately enhance the entire user experience.</p>
+  <div class="hero-image"></div>
+</section>
+
+<section class="p-cs p-cs-hero shopwell">
+  <h1>ShopWell</h1>
+  <div class="hero-image">
+    
+  </div>
+</section>
+
+
+
+</div><!-- portfolio-main -->
+<?php get_footer(); ?>
