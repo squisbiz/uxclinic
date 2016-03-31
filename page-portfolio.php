85ae5c6cd800328729b5362597299c3a0dace4ad
squisbiz <arturoruiz@Arturos-MacBook-Pro-5.local>
2016-03-31 10:41:29 -0700

    Merge remote-tracking branch 'origin/master'

--- page-portfolio.php
+++ page-portfolio.php
@@ -24,26 +24,81 @@ Template Name: PORTFOLIO
 
 <section class="p-uxteam">
 	<h2>UX Team</h2>
-	
 	<div class="team-container">
 		<div class="ux-team-member">
-			<img src="#" />
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ux-researcher-diana-arvayo.png" />
 			<a href="#">Diana Arvayo</a>
 			<h4>UX Researcher</h4>
 		</div>
-		<div class="ux-team-member"></div>
-		<div class="ux-team-member"></div>
 		
-		<div class="ux-team-member"></div>
-		<div class="ux-team-member"></div>
-		<div class="ux-team-member"></div>
+		<div class="ux-team-member">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ux-researcher-misael.png" />
+			<a href="#">Misael León</a>
+			<h4>UX Researcher</h4>
+		</div>
 		
-		<div class="ux-team-member"></div>
-		<div class="ux-team-member"></div>
-		<div class="ux-team-member"></div>
+		<div class="ux-team-member">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ux-researcher-denisse.png" />
+			<a href="#">Denisse Gutierrez</a>
+			<h4>UX Researcher</h4>
+		</div>
+		
+		<div class="ux-team-member">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ux-content-creator-jorge.png" />
+			<a href="#">Jorge Symonds</a>
+			<h4>Content Creator</h4>
+		</div>
+		<div class="ux-team-member">
+			<div class="ns-logo-party"></div>
+		</div>
+		
+		<div class="ux-team-member">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ux-business-developer-sandra.png" />
+			<a href="#">Sandra Vazquez</a>
+			<h4>Business Developer</h4>
+		</div>
+		
+		<div class="ux-team-member">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ui-designer-arturo.png" />
+			<a href="#">Arturo Ruiz</a>
+			<h4>UI - Front End Designer</h4>
+		</div>
+		
+		<div class="ux-team-member">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ui-designer-axel.png" />
+			<a href="#">Axel Valdez</a>
+			<h4>UI - Front End Designer</h4>
+		</div>
+		
+		<div class="ux-team-member">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ui-designer-miguel.png" />
+			<a href="#">Miguel Medina</a>
+			<h4>UX - UI Designer</h4>
+		</div>
 	</div>
-	
 </section>
+
+<section class="p-ux-process">
+	<h2>Nearsoft's UX Process</h2>
+	<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/ns-ux-process.png" />
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -51,11 +106,121 @@ Template Name: PORTFOLIO
 
 <section class="p-cs p-cs-hero shopwell">
   <h1>ShopWell</h1>
-  <div class="hero-image">
-    
-  </div>
+  <div class="hero-image"></div>
+  <p class="p-hero-description">ShopWell is a free website and iPhone/Android app that scores foods on how well they meet your unique needs.</p>
+  
+	<div class="p-challenge">
+		
+		<div class="p-challenge-description">
+			<h3>Challenge:</h3>
+			Increase user retention and engagement.
+		</div>
+		
+		<div class="p-challenge-before">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/shopwell-before.png" />
+			<h3>Before Our Research</h3>
+		</div>
+		
+	</div>
+	
+	<div class="p-research">
+		<h2>Research Process</h2>
+		<div class="p-research-description">
+			<h3>Remote Moderated Research</h3>
+			We conducted a Remote Moderated Research asking users a set of questions regarding how they feel and interact with the app.
+		</div>
+		
+		<div class="p-research-image"></div>
+		
+		<div class="p-research2-image"></div>
+		
+		<div class="p-research2-description">
+			<h3>Card sorting</h3>
+			And also, we did an activity called Card Sorting where users choose what features are most valuable for them.
+		</div>
+	</div>
+	
+	<div class="p-after">
+		<div class="p-after-image">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/shopwell-after.png" />
+		</div>
+		<h3>After Research Process</h3>
+		
+	</div>
+	
+	<div class="p-improvement">
+		<h3>Our UX Improvement</h3>
+		<ul>
+			<li>Personalized content for users</li>
+			<li>Social validation</li>
+		</ul>
+	</div>
 </section>
 
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="p-cs p-cs-hero cloudfinder">
+  <h1>CloudFinder</h1>
+  <div class="hero-image"></div>
+  <p class="p-hero-description">Cloudfinder is a simple, secure, and future-proof seamless solu on for cloud-to-cloud backup needs of MSPs, and VARs. Currently the service allows to back up major SaaS services, including Office 365, Salesforce, Google Apps, and Box.</p>
+  
+	<div class="p-challenge">
+		
+		<div class="p-challenge-description">
+			<h3>Challenge:</h3>
+			Redesign the old-looking interface and turn it into a more intui ve experience based on actual habits of use of channel partners.
+		</div>
+		
+		<div class="p-challenge-before">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/shopwell-before.png" />
+			<h3>Before Our Research</h3>
+		</div>
+		
+	</div>
+	
+	<div class="p-research">
+		<h2>Research Process</h2>
+		<div class="p-research-description">
+			<h3>Remote Moderated Research</h3>
+			We conducted a Remote Moderated Research asking users a set of questions regarding how they feel and interact with the app.
+		</div>
+		
+		<div class="p-research-image"></div>
+		
+		<div class="p-research2-image"></div>
+		
+		<div class="p-research2-description">
+			<h3>Card sorting</h3>
+			And also, we did an activity called Card Sorting where users choose what features are most valuable for them.
+		</div>
+	</div>
+	
+	<div class="p-after">
+		<div class="p-after-image">
+			<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/img/shopwell-after.png" />
+		</div>
+		<h3>After Research Process</h3>
+		
+	</div>
+	
+	<div class="p-improvement">
+		<h3>Our UX Improvement</h3>
+		<ul>
+			<li>Personalized content for users</li>
+			<li>Social validation</li>
+		</ul>
+	</div>
+</section>
 
 
 </div><!-- portfolio-main -->
